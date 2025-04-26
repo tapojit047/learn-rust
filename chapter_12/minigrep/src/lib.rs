f025b2fe6fbb2439f866ca932319a20d84c22f81
Tapajit Chandra Paul <tapajit@appscode.com>
2025-04-27 00:18:58 +0600

    Add chapter - 13(iii) Comparing Performance: Loops vs. Iterators
    Signed-off-by: Tapajit Chandra Paul <tapajit@appscode.com>

--- chapter_12/minigrep/src/lib.rs
+++ chapter_12/minigrep/src/lib.rs
@@ -8,20 +8,32 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();    // the first value in the return value of env::args is the name of the program
 
-        let mut ignore_case = false;
-        if args.len() == 4 {
-            ignore_case = args[3].to_lowercase() == "true";
-        } else{
-            println!("Loading env IGNORE_CASE...");
-            ignore_case = env::var("IGNORE_CASE").is_ok();
-        }
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Please provide a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Please provide a file path"),
+        };
+
+        let ignore_case = match args.next() {
+            Some(arg) => arg.to_lowercase() == "true",
+            None => {
+                println!("Loading env IGNORE_CASE...");
+                env::var("IGNORE_CASE").is_ok()
+            }
+        };
+        // if args.len() == 4 {
+        //     ignore_case = args[3].to_lowercase() == "true";
+        // } else{
+        //     println!("Loading env IGNORE_CASE...");
+        //     ignore_case = env::var("IGNORE_CASE").is_ok();
+        // }
 
         Ok(Config {
             query,
@@ -49,24 +61,31 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    let query = query.to_lowercase();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    // let mut results = Vec::new();
+    // let query = query.to_lowercase();
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+
+    contents.lines().filter(|line| line.to_lowercase().contains(query)).collect()
 }
 
 #[cfg(test)]
--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -2,9 +2,7 @@ use std::{env, process};
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -20,3 +18,5 @@ fn main() {
 
 // TO RUN:
 // $ cargo run -- body poem.txt
+
+// Remember that the first value in the return value of env::args is the name of the program.
